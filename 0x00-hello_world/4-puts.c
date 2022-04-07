6124a9938c8da329fc71827ad2f30d72c1b6fa02
lewis-otwori <lewisotwori@gmail.com>
2022-04-07 12:46:28 -0700

    my first 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc $CFILE -S
+
+#include <stdio.h>
+/**
+ * main -Entry point
+ * 
+ *Return : Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
+}
+
