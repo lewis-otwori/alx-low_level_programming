6c3e6d3faae30071fd8a096813f59d6a905e9ba6
lewis-otwori <lewisotwori@gmail.com>
2022-04-08 23:47:34 -0700

    my first 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  *Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
