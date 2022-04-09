90ae27f2ac304cbce8ba2b64961a0a9e5372d7e8
lewis-otwori <lewisotwori@gmail.com>
2022-04-09 13:26:33 -0700

    my first 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet in lowercase then uppercase then uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+ 
