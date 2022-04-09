f06a20e216e91b92fedb0f582b6d0de8c77b13a0
lewis-otwori <lewisotwori@gmail.com>
2022-04-09 14:18:01 -0700

    my first 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all digit numbers of base 10 using putchar
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x< 58; c++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
