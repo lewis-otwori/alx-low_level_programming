b1a9de52453caa217461f5d6fb01c0ba33680884
lewis-otwori <lewisotwori@gmail.com>
2022-04-09 08:58:11 -0700

    my first 0-positive_or_negative.c file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlibi.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - int n is assigned a random number everytime
+ * its executed then printed
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/*code - is positive or negative*/
+	if (n > 0)
+	{
+		printf("%d is positive\n" , n);
+	}
+	else if ( == 0)
+	{
+		printf("%d is zero\n" , n);
+	}
+	else
+	{
+		printf("%d is negative\n , n);
+	}
+	return (0);
+
+
