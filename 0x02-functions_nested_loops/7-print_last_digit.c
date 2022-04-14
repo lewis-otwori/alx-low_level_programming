0f5868a14a3973e08bcb1692b4bb66d372e38174
lewis-otwori <lewisotwori@gmail.com>
2022-04-14 03:52:10 -0700

    my first 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _abs - checks if number is positive or negative 
+ * @n: integer to be tested
+ * Return: returns value of abs(n)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: integer whose last digit will be printed
+ * Return: last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	int i;
+
+	i = _abs((n % 10));
+	_putchar(i + 48);
+	return (i);
+}
