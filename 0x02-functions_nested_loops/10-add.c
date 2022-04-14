f20d2259c2c091328bc302425d313dfce3461cd8
lewis-otwori <lewisotwori@gmail.com>
2022-04-14 09:39:27 -0700

    my first 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: returns x+y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
