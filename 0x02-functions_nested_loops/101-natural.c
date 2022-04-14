e88831fa5a34674d0f6b371e2ce4283cd654cc94
lewis-otwori <lewisotwori@gmail.com>
2022-04-14 12:35:22 -0700

    my first 101-natural.c file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - main block 
+ * main - main block
  * Description: computes and prints the sum of all the multiples of 3 or
  * s below 1024 (excluded), followed by a new line
  * Return: 0
