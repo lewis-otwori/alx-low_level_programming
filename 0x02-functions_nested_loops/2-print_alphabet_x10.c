7cc61fa5cc870101453c4cec4dd9ddc44fff19ca
lewis-otwori <lewisotwori@gmail.com>
2022-04-13 23:37:47 -0700

    my first 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times alphabet in lowercase 
+ *
+ * void has no return value
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,39 @@
+/*prints _putchar*/
+int _putchar(char c);
+
+/*prints the alphabet*/
+void print_alphabet(void);
+
+/*prints 10 times the alphabet*/
+void print_alphabet_x10(void);
+
+/*checks for lowercase character*/
+int _islower(int c);
+
+/*checks for alphabetic character*/
+int _isalpha(int c);
+
+/*prints the sign of a number */
+int print_sign(int n);
+
+/*computes the absolute value of an integer*/
+int _abs(int);
+
+/*prints the last digit of a number*/
+int print_last_digit(int);
+
+/*prints every minute of the day of jack bauer*/
+void times_table(void);
+
+/*prints the 9 times table*/
+void times_table(void);
+
+/*add two integers and returns the results*/
+int add(int, int);
+
+/*prints all natural numbers from n to 98*/
+void print_to_98(int n);
+
+/*prints the n times tables*/
+void print_times_table(int n);
+
