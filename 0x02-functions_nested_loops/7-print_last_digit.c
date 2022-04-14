530b6b343fa0ad691d4609c5ce6c4df9be738c17
lewis-otwori <lewisotwori@gmail.com>
2022-04-14 03:54:09 -0700

    my first 7-print_last_digit.c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs - checks if number is positive or negative 
+ * _abs - checks if number is positive or negative
  * @n: integer to be tested
  * Return: returns value of abs(n)
  */
