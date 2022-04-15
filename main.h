44122d1146e2d4f53f131a7dcae5ce085a669817
lewis-otwori <lewisotwori@gmail.com>
2022-04-14 23:47:09 -0700

    my first 0-isupper.c file

---
+++ main.h
@@ -0,0 +1,41 @@
+/*Function prototype*/
+
+/*prints _putchar*/
+int _putchar(char c);
+
+/*checks for uppercase character*/
+int _isupper(int c);
+
+/*checks for a digit(0 through 9)*/
+int _isdigit(int c);
+
+/*multiplies two integers*/
+int mul(int a, int b);
+
+/*prints the numbers from 0 to 9*/
+void print_numbers(void);
+
+/*prints most numbers from 0 to 9*/
+void print_most_numbers(void);
+
+/*prints 10 times the numbers from 0 to 14*/
+void more_numbers(void);
+
+/*draws a straight line*/
+void print_line(int n );
+
+/*draws a diagonal line*/
+void print_diagonal(int n);
+
+/*prints a square*/
+void print_square(int size);
+
+/*prints a triangle*/
+void print_triangle(int size);
+
+/*prints an integer*/
+void print_number(int n);
+
+
+
+
