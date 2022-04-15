e2a30a4c43b734da5893c37836c97b7c887067ba
lewis-otwori <lewisotwori@gmail.com>
2022-04-15 01:01:08 -0700

    my first 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is uppercase
+ * @c: represent character
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
