f72d669de131e6b64b7477db226a20385c038532
lewis-otwori <lewisotwori@gmail.com>
2022-04-26 12:35:12 -0700

    my first 1-memcpy.c file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @src: source memory area
+ * @dest: destination memory area
+ * @n: bytes filled
+ * Return: the pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+
+	return (dest);
+}
