1618ff70263d320a2ba403763f3a5c4a5f49b8df
lewis-otwori <lewisotwori@gmail.com>
2022-04-28 23:09:19 -0700

    my first 4-strpbrk.c file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any set of bytes
+ * @s: string
+ * @accept: string to match
+ * Return: pointer to the byte in s that matches one of the bytes in accept */
+char *_strpbrk(char *s, char *accept)
+{
+	int m, n;
+	char *ch;
+
+	m = 0;
+	while (s[m] != '\0')
+	{
+		n = 0;
+		while (accept[n] != '\0')
+		{
+			if (accept[n] == s[m])
+			{
+				ch = &s[m];
+				return (ch);
+			}
+			n++;
+		}
+		m++;
+	}
+	return (0);
+}
