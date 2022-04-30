bf56fd3446d7649cdabd08c36510597b40337f2f
lewis-otwori <lewisotwori@gmail.com>
2022-04-30 06:43:10 -0700

    my first 0-puts_recursion.c file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,57 @@
+/*function that prints a string*/
+
+void _puts_recursion(char *s);
+
+
+
+/*function that prints a string in reverse*/
+
+void _print_rev_recursion(char *s);
+
+
+
+/*funtion that returns the length of a string*/
+
+int _strlen_recursion(char *s);
+
+
+
+/*function that returns the factorial of a given number*/
+
+int factorial(int n);
+
+
+
+/*function that returns the value of x raised to the power y*/
+
+int _pow_recursion(int x, int y);
+
+
+
+/*function that returns the natural square root of a number*/
+
+int _sqrt_recursion(int n);
+
+
+
+/*function that returns 1 if the input integer is a prime number*/
+
+int is_prime_number(int n);
+
+
+
+/*function that returns 1 if a string is a palindrome and 0 if not*/
+
+int is_palindrome(char *s);
+
+
+
+/*function that compares two strings and returns 1 if the strings can be considered identical, otherwise 0*/
+
+int wildcmp(char *s1, char *s2);
+
+
+
+/*function that prints _putchar*/
+
+int _putchar(char c);
