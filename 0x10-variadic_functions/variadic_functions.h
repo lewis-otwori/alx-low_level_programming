1cd3bb02249bffc5a8695682836ada8776ca79c6
lewis-otwori <lewisotwori@gmail.com>
2022-05-17 09:31:25 -0700

    my variadic_functions.h file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,16 @@
 /* putchar function*/
 int _putchar(char c);
 
-/* function that returns the sum of all its parameters*/
+/*function that returns the sum of all its parameters*/
 int sum_them_all(const unsigned int n, ...);
+
+/*functions that prints numbers, followed by a new line*/
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+/*function that prints strings, followed by a new line*/
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/*function that prints anything*/
+void print_all(const char * const format, ...);
 
 #endif
