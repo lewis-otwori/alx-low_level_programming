cddd9efeeb76f48a8b858d7775ec88d65c778668
Lewis Otwori <lewisotwori@gmail.com>
2022-05-24 23:41:08 +0300

    0x12-singly_linked_lists project

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,32 @@
+#ifndef _SINGLY_LINKED_LISTS
+#define _SINGLY_LINKED_LISTS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+/**
+ * struct list_s - singly linked lists
+ * @str: malloc'ed string
+ * @len: length of the string
+ * @next; points to the next node
+ *
+ * Descriotion: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+int _strlen(const char *);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+void __attribute__((constructor)) print_first(void);
+
+#endif /*LIST_H*/
